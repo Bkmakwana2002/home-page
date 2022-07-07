8c1f916b13e0d019634f848c9c1676b1b8fbd4d4
Bkmakwana2002 <2020meb1278@iitrpr.ac.in>
2022-07-08 01:36:14 +0530

    first commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <section class="hero">
+    <div class="hero__image-wrapper">
+    </div>
+    <div class="hero__text">
+      <h1>AIR 6</h1>
+      <p>6th International Conference of The Robotics Society</p>
+      <button id="hero__img-refresh">Learn More</button>
+    </div>
+  </section>
+  )
+}
+
+export default Hero
