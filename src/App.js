a507b6c5f18d85b6c3ab89817c97f341633c2cb1
Bkmakwana2002 <2020meb1278@iitrpr.ac.in>
2022-07-08 18:56:37 +0530

    added taiwlind

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css'
-import Navbar from './components/Navbar/Navbar';
-import Hero from './components/Hero/Hero';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Hero/>
+
     </div>
   );
 }
